9c8afc42ddf2610ad1f4127f402a81699179e2a4
EloiseSchachterle <eloises3@nycstudents.net>
2025-09-26 15:35:08 -0400

    Appointment book

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    boolean[][] schedule = new boolean[8][60];
+    AppointmentBook a = new AppointmentBook(schedule);
+    for(int i = 10; i < 15; i++) schedule[1][i] = true;
+}
