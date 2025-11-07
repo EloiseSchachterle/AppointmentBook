797c5e904fbb440ae501a14ce54864e0858a28fa
EloiseSchachterle <eloises3@nycstudents.net>
2025-11-06 23:30:18 -0500

    Appointmentbook

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public record AppointmentBook(boolean[][] schedule) {
     private boolean isMinuteFree(int period, int minute) {
         return schedule[period - 1][minute];
@@ -25,11 +27,7 @@ public record AppointmentBook(boolean[][] schedule) {
     }
 
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
-//        for(int i = 25; i < 30; i++) schedule[1][i] = true;
-//        for(int i = 0; i < 15; i++) schedule[2][i] = true;
-//        for(int i = 41; i < 60; i++) schedule[2][i] = true;
-//        for(int i = 5; i < 30; i++) schedule[3][i] = true;
-//        for(int i = 44; i < 60; i++) schedule[3][i] = true;
+
         for (int i = startPeriod; i <= endPeriod; i++) {
             int freeBlock = findFreeBlock(i, duration);
             if (freeBlock > -1) {
@@ -45,9 +43,6 @@ public record AppointmentBook(boolean[][] schedule) {
         for (int i = 0; i < schedule[period - 1].length; i++)
             System.out.println(i + " " + schedule[period - 1][i]);
     }
-//    public void reserveBlock(int startMinute, int duration, int period) {
-//        for (int i = startMinute; i < startMinute + duration; i++){
-//            schedule[period - 1][i] = false;}
-//
-//    }
+
+
 }
